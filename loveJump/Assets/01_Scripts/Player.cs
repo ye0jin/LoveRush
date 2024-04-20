80129ecd87d636f01a65394429e6ece5ebc47b90
ye0jin <fruitfighterlim@gmail.com>
2024-04-20 15:38:58 +0900

    asd

---
+++ loveJump/Assets/01_Scripts/Player.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private bool IsJump = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+    }
+}
